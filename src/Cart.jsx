d6304fd8f6fc0ef4743979ed204e2ad2efc2435d
pelzmagic <akinloyepelz@gmail.com>
2025-03-18 01:08:26 +0100

    feat: add style to cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -21,14 +21,15 @@ export default function Cart() {
 function AddToCartItems() {
   return (
     <div>
-      <div>
-        <p>Classic Tiramisu</p>
-        <div>
-          <p>1x</p>
-          <p>@5.50</p>
-          <p>$5.50</p>
+      <div className="space-y-2 border border-blue-500">
+        <p className="text-rose-900 text-xsm leading-[150%] font-semibold">Classic Tiramisu</p>
+        <div className="w-2/5 border border-red flex justify-between">
+          <p className="text-red text-xsm leading-[150%] font-semibold">1x</p>
+          <p className="text-rose-500 leading-[150%] text-xsm">@5.50</p>
+          <p className="text-rose-500 leading-[150%] font-semibold text-xsm">$5.50</p>
         </div>
       </div>
+      <img src="/Button - Remove Item.png" alt="delete button" />
     </div>
   );
 }
