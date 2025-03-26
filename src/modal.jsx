7349cacc85fe6161356973b3d745e9bc6cf4feb1
pelzmagic <akinloyepelz@gmail.com>
2025-03-26 01:51:15 +0100

    add modal

--- src/modal.jsx
+++ src/modal.jsx
@@ -3,9 +3,10 @@ export default function Modal() {
     <div className="fixed top-0 bg-black w-full min-h-screen opacity-80 flex justify-center items-center">
       <div className="p-10 bg-white rounded-xl">
         <img src="/checkmark.svg" alt="checkmark" />
-        <p className="text-rose-900 font-bold text-[40px] leading-[120%]">Order Confirmed</p>
-        <p className="text-rose-500 text-base">We hope you enjoy your food!</p>
+        <p className="text-rose-900 font-bold text-[40px] leading-[120%] mt-6">Order Confirmed</p>
+        <p className="text-rose-500 text-base mt-2">We hope you enjoy your food!</p>
         <ModalItems />
+        <button className="w-full bg-red px-6 py-4 rounded-full text-white text-base leading-[150%] font-semibold mt-8">Start New Order</button>
       </div>
     </div>
   );
@@ -13,19 +14,24 @@ export default function Modal() {
 
 function ModalItems() {
   return (
-    <div className="bg-rose-50 p-6">
+    <div className="bg-rose-50 p-6 rounded-lg">
       <div className="flex justify-between items-center">
         <div className="flex gap-4 border border-red">
           <img src="/image-tiramisu-desktop.jpg" alt="tiramisu" className="w-12 aspect-square rounded" />
           <div className="space-y-2">
             <p className="text-rose-900 font-semibold text-xsm leading-[150%]">Classic Tiramisu</p>
-            <div className="flex">
+            <div className="flex gap-3 items-center">
               <p className="text-red">1x</p>
-              <p>@$5.50</p>
+              <p className="text-rose-500 text-xsm leading-[150%]">@$5.50</p>
             </div>
           </div>
         </div>
-        <p>$5.50</p>
+        <p className="text-base leading-[150%] text-rose-900 font-semibold">$5.50</p>
+      </div>
+      <hr className="border border-rose-100 mt-4" />
+      <div className="flex justify-between items-center border border-red mt-6">
+        <p className="text-xsm leading-[150%] text-rose-900">Order Total</p>
+        <p className="text-2xl leading-[125%] font-bold">$46.50</p>
       </div>
     </div>
   );
